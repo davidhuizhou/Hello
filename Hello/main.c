04cb0e62cb7a5f06871fc2c842c2d16b12ce6478
Hui Zhou <huizhou@Huis-MacBook-Pro.local>
2012-11-27 22:12:52 -0500

    Initial Commit

---
+++ Hello/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Hello
+//
+//  Created by Hui Zhou on 11/27/12.
+//  Copyright (c) 2012 Hui Zhou. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
